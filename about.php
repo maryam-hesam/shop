5d26fcb3343c4d1c55cfc407d248fda64fac597c
maryam <maryam.n.1985@gmail.com>
2021-09-27 09:39:04 +0330

    =add all file to git

---
+++ about.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat harum tempora voluptatem. Accusantium ad alias at corporis deserunt eaque excepturi in minima molestias, neque obcaecati, odio quae quasi sint totam.</p>
+</body>
+</html>
